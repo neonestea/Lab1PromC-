6b3c854143889913e1543bceff5e58a4a4df7eae
neonestea <44156406+neonestea@users.noreply.github.com>
2023-05-28 16:15:07 +0300

    Update test.cpp

--- HW1_2/testsuite/libsrc/array/src/test.cpp
+++ HW1_2/testsuite/libsrc/array/src/test.cpp
@@ -13,79 +13,48 @@ TEST_CASE("CheckSize", "[array]")
 
 }
 
+TEST_CASE("CheckRandomAccess", "[array]")
+{
+    MyArray<int, MAX> arr = {1, 2, 3, 4, 5, 6};
+    CHECK(arr[0] == 1);
+    CHECK(arr[1] == 2);
+    CHECK(arr[2] == 3);
+    CHECK(arr[3] == 4);
+    CHECK(arr[4] == 5);
+    CHECK(arr[5] == 6);
+
+}
+
 TEST_CASE("CheckFront", "[array]")
 {
     MyArray<int, MAX> arr = {0, 0, 0, 0, 0, 0};
     arr.front() = 1;
     CHECK(arr.front() == 1);
+    //arr.frontConst() = 14; ошибка компиляции, правильно
+    CHECK(arr.front() == 1);
+    CHECK(arr.frontConst() == 1);
 }
 
-TEST_CASE("CheckBack", "[array]") 
+TEST_CASE("CheckBack", "[array]")
 {
     MyArray<int, MAX> arr = {0, 0, 0, 0, 0, 0};
-    arr.back() = 6;
-    CHECK(arr.back() == 6);
+    arr.back() = 1;
+    CHECK(arr.back() == 1);
+    //arr.backConst() = 100;  ошибка компиляции, правильно
+    CHECK(arr.backConst() == 1);
+    CHECK(arr.back() == 1);
 }
 
-TEST_CASE("CheckIterator", "[array]") 
-{ 
-    MyArray<int, MAX> arr = {0, 0, 0, 0, 0, 0};
-    arr[0] = 1;
-    arr[1] = 2;
-    arr[2] = 3;
-    arr[3] = 4;
-    arr[4] = 5;
-    arr[5] = 6;
-    int valueToCheck = 1;
-    for (auto it = arr.begin(), end = arr.end(); it != end; ++it) { 
-        const auto i = *it; 
-        std::cout << i << "\n";
-        CHECK(i == valueToCheck);
-        valueToCheck++;
-    }
-}
-
-
-TEST_CASE("CheckReverseIterator", "[array]")
+TEST_CASE("CheckAt", "[array]")
 {
-    MyArray<int, MAX> arr = {0, 0, 0, 0, 0, 0};
-    arr[0] = 1;
-    arr[1] = 2;
-    arr[2] = 3;
-    arr[3] = 4;
-    arr[4] = 5;
-    arr[5] = 6;
-    int valueToCheck = 6;
-    for (auto it = arr.rbegin(), end = arr.rend(); it != end; --it) { 
-        const auto i = *it; 
-        std::cout << i << "\n";
-        CHECK(i == valueToCheck);
-        valueToCheck--;
-    }
-
-}
-
-TEST_CASE("CheckAt", "[array]")     
-{
-    MyArray<int, MAX> arr = {0, 0, 0, 0, 0, 0};
+    MyArray<int, MAX> arr = {0, 0};
     CHECK_THROWS_AS(arr.at(98), std::invalid_argument);
-    
-}
-
-
-TEST_CASE("CheckEmpty", "[array]")
-{
-    MyArray<int, MAX> arr = {0, 0, 0, 0, 0, 0};
-    arr[1] = 2;
-    arr[2] = 3;
-    arr[3] = 4;
-    arr[4] = 5;
-    CHECK_FALSE(arr.empty());
-
+    arr[0] = 3;
+    CHECK(arr.at(0) == 3);
 
 }
 
-TEST_CASE("CheckEquals", "[array]")         
+TEST_CASE("CheckEquals", "[array]")
 {
     MyArray<int, MAX> arr = {0, 0, 0, 0, 0, 0};
     MyArray<int, MAX> arr2 = {0, 0, 0, 0, 0, 0};
@@ -111,8 +80,6 @@ TEST_CASE("CheckEquals", "[array]")
     arr3[4] = 2;
     arr3[5] = 1;
 
-    std::cout << arr << std::endl;
-    std::cout << arr2 << std::endl;
     bool eq12 = arr == arr2;
     bool neq12 = arr != arr2;
     bool g13 = arr > arr3;
@@ -127,3 +94,131 @@ TEST_CASE("CheckEquals", "[array]")
     CHECK(se13 == 1);
 
 }
+
+
+TEST_CASE("CheckIterator", "[array]")
+{
+    MyArray<int, MAX> arr = {0, 0, 0, 0, 0, 0};
+    arr[0] = 1;
+    arr[1] = 2;
+    arr[2] = 3;
+    arr[3] = 4;
+    arr[4] = 5;
+    arr[5] = 6;
+
+
+    int valueToCheck = 6;
+    //std::cout << *arr.end();
+    //std::cout << *arr.begin();
+    for (auto it = --arr.end(), end = arr.begin(); it != end; --it) {
+        const auto i = *it;
+    
+        CHECK(i == valueToCheck);
+        valueToCheck--;
+    }
+
+    valueToCheck = 1;
+    for (auto it = arr.begin(), end = arr.end(); it != end; ++it) { 
+        
+        auto i = *it; 
+        CHECK(i == valueToCheck);
+        valueToCheck++;
+    }
+    *arr.begin() = 100;
+    *arr.end() = 200;
+    CHECK(*arr.begin() == 100);
+    CHECK(*arr.end() == 200);
+
+}
+
+
+TEST_CASE("CheckReverseIterator", "[array]")
+{
+    MyArray<int, MAX> arr = {0, 0, 0, 0, 0, 0};
+    arr[0] = 1;
+    arr[1] = 2;
+    arr[2] = 3;
+    arr[3] = 4;
+    arr[4] = 5;
+    arr[5] = 6;
+    int valueToCheck = 6;
+    
+    for (auto it = arr.rbegin(), end = arr.rend(); it != end; ++it) { 
+        auto i = *it; 
+        std::cout << "Is " << i << " should be " << valueToCheck << "\n";
+        
+        CHECK(i == valueToCheck);
+        valueToCheck--;
+    }
+    valueToCheck = 1;
+   
+    for (auto it = arr.rend(), end = arr.rbegin(); it != end; --it) {
+        const auto i = *it;
+        std::cout << "Is " << i << " should be " << valueToCheck << "\n";
+        CHECK(i == valueToCheck);
+        valueToCheck++;
+    }
+    *arr.rbegin() = 100;
+    *arr.rend() = 200;
+    CHECK(*arr.rbegin() == 100);
+    CHECK(*arr.rend() == 200);
+
+}
+
+TEST_CASE("CheckConstIterator", "[array]")
+{
+    MyArray<int, MAX> arr = {0, 0, 0, 0, 0, 0};
+    arr[0] = 1;
+    arr[1] = 2;
+    arr[2] = 3;
+    arr[3] = 4;
+    arr[4] = 5;
+    arr[5] = 6;
+
+    std::cout << arr << "\n";
+    int valueToCheck = 1;
+    for (auto it = arr.begin(), end = arr.end(); it != end; ++it) { 
+        auto i = *it; 
+        std::cout << i << "\n";
+        CHECK(i == valueToCheck);
+        valueToCheck++;
+    }
+   
+}
+
+
+TEST_CASE("CheckConstReverseIterator", "[array]")
+{
+    MyArray<int, MAX> arr = {0, 0, 0, 0, 0, 0};
+    arr[0] = 1;
+    arr[1] = 2;
+    arr[2] = 3;
+    arr[3] = 4;
+    arr[4] = 5;
+    arr[5] = 6;
+    int valueToCheck = 6;
+    for (auto it = arr.rbegin(), end = arr.rend(); it != end; ++it) { 
+        const auto i = *it; 
+        std::cout << i << "\n";
+        CHECK(i == valueToCheck);
+        valueToCheck--;
+    }
+   
+
+}
+
+
+
+
+TEST_CASE("CheckEmpty", "[array]")
+{
+    MyArray<int, MAX> arr = {0, 0, 0, 0, 0, 0};
+    arr[1] = 2;
+    arr[2] = 3;
+    arr[3] = 4;
+    arr[4] = 5;
+    CHECK_FALSE(arr.empty());
+
+
+}
+
