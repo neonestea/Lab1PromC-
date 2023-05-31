aac8ec6d73ebad11e8a386ed9de82207e3ba508f
neonestea <44156406+neonestea@users.noreply.github.com>
2023-05-31 16:22:35 +0300

    Update test.cpp

--- HW1_2/testsuite/libsrc/array/src/test.cpp
+++ HW1_2/testsuite/libsrc/array/src/test.cpp
@@ -1,209 +1,332 @@
-#include <catch2/catch_test_macros.hpp>
+#pragma once
+
 #include <iostream>
-#include "array/array.hpp"
-#include <string>
+#include <stdexcept>
+#include <iterator>
 
-#define MAX 6
 
-void createArray() {
-    MyArray<int, MAX> arr({0, 0, 0, 0, 0, 0, 5, 54});
-}
-TEST_CASE("CheckSize", "[array]")
+template <class T, size_t N>
+class MyArray
 {
+    class iterator 
+    {
+        friend class MyArray;
+        using value_type = T;
+        using reference = T&;
+        using pointer = T*;
+        friend class MyArray;
+        
+    private:
+        pointer ptr_;
+        iterator(pointer ptr) : ptr_(ptr) {}
+    public:
     
-    
-    CHECK_THROWS_AS(createArray(), std::invalid_argument);
-    
-}
-
-TEST_CASE("CheckInitialization", "[array]")
-{
-    MyArray<int, MAX> arr({0, 0, 0, 0, 0, 0});
-    CHECK(arr.size() == 6); //size
-
-}
-
-TEST_CASE("CheckRandomAccess", "[array]")
-{
-    MyArray<int, MAX> arr({1, 2, 3, 4, 5, 6});
-     for (int i = 0; i < MAX; ++i) {
-         CHECK(arr[i] == i + 1);
-    }
-
-}
-
-TEST_CASE("CheckFront", "[array]")
-{
-    MyArray<int, MAX> arr({0, 0, 0, 0, 0, 0});
-    arr.front() = 1;
-    CHECK(arr.front() == 1);
-    //arr.frontConst() = 14; ошибка компиляции, правильно
-    CHECK(arr.front() == 1);
-    CHECK(arr.frontConst() == 1);
-}
-
-TEST_CASE("CheckBack", "[array]")
-{
-    MyArray<int, MAX> arr({0, 0, 0, 0, 0, 0});
-    arr.back() = 1;
-    CHECK(arr.back() == 1);
-    //arr.backConst() = 100;  ошибка компиляции, правильно
-    CHECK(arr.backConst() == 1);
-    CHECK(arr.back() == 1);
-}
-
-TEST_CASE("CheckAt", "[array]")
-{
-    MyArray<int, 2> arr({0, 0});
-    CHECK_THROWS_AS(arr.at(98), std::invalid_argument);
-    arr[0] = 3;
-    CHECK(arr.at(0) == 3);
-
-}
-
-TEST_CASE("CheckEquals", "[array]")
-{
-    MyArray<int, MAX> arr({0, 0, 0, 0, 0, 0});
-    MyArray<int, MAX> arr2({0, 0, 0, 0, 0, 0});
-    MyArray<int, MAX> arr3({0, 0, 0, 0, 0, 0});
-    arr.front() = 1;
-    arr.back() = 6;
-
-    for (int i = 0; i < MAX; ++i) {
-        if (i != 0 && i != MAX) {
-            arr[i] = i + 1;
+    	
+    	reference operator*() const { 
+            //iterator tmp = *ptr_; 
+            return *ptr_; 
         }
-        arr2[i] = i + 1;
-        arr3[i] = MAX - i;
-    }
-    std::cout << arr << std::endl;
-    std::cout << arr2 << std::endl;
-    std::cout << arr3 << std::endl;
-
-    bool eq12 = arr == arr2;
-    bool neq12 = arr != arr2;
-    bool g13 = arr > arr3;
-    bool ge13 = arr >= arr3;
-    bool s13 = arr < arr3;
-    bool se13 = arr <= arr3;
-    CHECK(eq12 == 1);
-    CHECK(neq12 == 0);
-    CHECK(g13 == 0);
-    CHECK(ge13 == 0);
-    CHECK(s13 == 1);
-    CHECK(se13 == 1);
-
-}
-
-
-TEST_CASE("CheckIterator", "[array]")
-{
-    MyArray<int, MAX> arr({0, 0, 0, 0, 0, 0});
-    for (int i = 0; i < MAX; ++i) {
-        
-        arr[i] = i + 1;
-    }
-
-
-    int valueToCheck = 6;
-    for (auto it = arr.end(), end = arr.begin(); it != end; --it) {
-        const auto i = *it;
+        pointer operator->() { return ptr_; }
     
-        CHECK(i == valueToCheck);
-        valueToCheck--;
-    }
-
-    valueToCheck = 1;
-    for (auto it = arr.begin(), end = arr.end(); it != end; ++it) { 
-        
-        auto i = *it; 
-        CHECK(i == valueToCheck);
-        valueToCheck++;
-    }
-    *arr.begin() = 100;
-    *arr.end() = 200;
-    CHECK(*arr.begin() == 100);
-    CHECK(*arr.end() == 200);
-
-}
-
-
-TEST_CASE("CheckReverseIterator", "[array]")
-{
-    MyArray<int, MAX> arr({0, 0, 0, 0, 0, 0});
-    for (int i = 0; i < MAX; ++i) {
-        
-        arr[i] = i + 1;
-    }
-    int valueToCheck = 6;
+        // Prefix increment
+        iterator& operator++() { ++ptr_; return *this; }  
     
-    for (auto it = arr.rbegin(), end = arr.rend(); it != end; ++it) { 
-        auto i = *it; 
+        // Postfix increment
+        iterator operator++(int) 
+        { 
+            iterator tmp = *this; 
+            ++(*this); 
+            return tmp; 
+            
+        }
+
+        /*iterator& operator--() { 
+            --ptr_; 
+            return *this; 
+        }  
+    
+        // Postfix increment
+        iterator operator--(int) 
+        { 
+            iterator tmp = *this; 
+            --(*this); 
+            return tmp; 
+            
+        }
+
+        iterator operator[](int n) const { //random Access, доступ О(1)
+            iterator tmp = *this; 
+            tmp += n;
+            return tmp;
+        }
         
-        CHECK(i == valueToCheck);
-        valueToCheck--;
-    }
-    valueToCheck = 1;
-   
-    for (auto it = arr.rend(), end = arr.rbegin(); it != end; --it) {
-        const auto i = *it;
-        CHECK(i == valueToCheck);
-        valueToCheck++;
-    }
-    *arr.rbegin() = 100;
-    *arr.rend() = 200;
-    CHECK(*arr.rbegin() == 100);
-    CHECK(*arr.rend() == 200);
+        
+        iterator& operator+=(std::ptrdiff_t n) { //смещаем указатель
+            
+            ptr_ += n; 
+            return *this;
+        }*/
 
-}
 
-TEST_CASE("CheckConstIterator", "[array]")
+        bool operator== (const iterator& b) { return ptr_ == b.ptr_; };
+        bool operator!= (const iterator& b) { return ptr_ != b.ptr_; };   
+    
+    };
+    class reverseIterator 
+    {
+        friend class MyArray;
+        using value_type = T;
+        using reference = T&;
+        using pointer = T*;
+    private:
+        pointer ptr_;
+        reverseIterator(pointer ptr) : ptr_(ptr) {}
+    public:
+    
+    	
+    	reference operator*() const { 
+            //reverseIterator tmp = *this;
+            
+            return *ptr_; 
+        }
+
+        pointer operator->() { return ptr_; }
+    
+        // Prefix increment
+        reverseIterator& operator++() { --ptr_; return *this; }  
+    
+        // Postfix increment
+        reverseIterator operator++(int) 
+        { 
+            reverseIterator tmp = *this; 
+            --(*this); 
+            return tmp; 
+            
+        }
+
+        /*reverseIterator& operator--() { ++ptr_; return *this; }  
+    
+        // Postfix increment
+        reverseIterator operator--(int) 
+        { 
+            reverseIterator tmp = *this; 
+            
+            ++(*this); 
+            return tmp; 
+            
+        }
+
+        reverseIterator operator[](int n) const { //random Access, доступ О(1)
+            iterator tmp = *this; 
+            tmp -= n;
+            return tmp;
+        }
+        
+        
+        reverseIterator& operator-=(std::ptrdiff_t n) { //смещаем указатель
+            
+            ptr_ -= n; 
+            return *this;
+        }*/
+    
+        bool operator== (const reverseIterator& b) { return ptr_ == b.ptr_; };
+        bool operator!= (const reverseIterator& b) { return ptr_ != b.ptr_; };   
+    
+    };
+
+private:
+    using value_type = T;
+    using reference = value_type&;
+    value_type data_[N];
+    typedef const iterator constIterator;
+    typedef const reverseIterator constReverseIterator;
+    
+public:
+    explicit MyArray (const std::initializer_list<T>& list)
+    {
+        if (list.size() != N) {
+            throw std::invalid_argument("Incorrect size of initializer list");   
+        }
+        std::copy(list.begin(), list.end(), data_);
+        
+    };
+
+    MyArray ()
+    {
+        
+    };
+    
+    
+    iterator begin() 
+	{
+		return iterator(&data_[0]);
+    }
+
+	iterator end() {
+		return iterator(&data_[N]); 
+    }
+    
+    reverseIterator rbegin() 
+	{
+		return reverseIterator(&data_[N - 1]);
+    }
+
+	reverseIterator rend() {
+		return reverseIterator(&data_[0]);
+    }
+    
+    constIterator beginConst() const
+	{
+		return constIterator(&data_[0]);
+    }
+
+	constIterator endConst() const
+	{
+		return constIterator(&data_[N]);
+    }
+    
+    constReverseIterator rbeginConst() const
+	{
+		return constReverseIterator(&data_[N - 1]);
+    }
+
+	constReverseIterator rendConst() const 
+	{
+		return constReverseIterator(&data_[0] - 1);
+    }
+
+    bool operator==(const MyArray& other) const
+    {
+        return (N == other.size() && std::equal(data_, data_ + N, other.data_));
+    }
+    
+    bool operator!=(const MyArray& other) const
+    {
+        return (N == other.size() && !std::equal(data_, data_ + N, other.data_));
+    }
+    
+    bool operator>(const MyArray& other) const
+    {
+        size_t n1 = N;
+        size_t n2 = other.size();
+        return !std::lexicographical_compare(data_, data_ + n1, other.data_, other.data_ + n2);
+    }
+    
+    bool operator<(const MyArray& other) const
+    {
+        size_t n1 = N;
+        size_t n2 = other.size();
+        return std::lexicographical_compare(data_, data_ + n1, other.data_, other.data_ + n2);
+    }
+    
+    bool operator>=(const MyArray& other) const
+    {
+        
+        if (*this == other || *this > other)
+        {
+            return true;
+        }
+        return false;
+    }
+    
+    bool operator<=(const MyArray& other) const
+    {
+        
+         if (*this == other || *this < other)
+        {
+            return true;
+        }
+        return false;
+    }
+    
+    value_type& operator[](int i)
+    {
+        return data_[i];
+        //auto it = iterator(&data_[0]);
+        //return *it[i];
+    }
+    
+    value_type& at(int i) {
+    	// Проверяем выход за пределы массива
+    	if (i >= int(size())) {
+		throw std::invalid_argument("Element not exist");
+	}
+        return data_[i];
+        
+    }
+    
+    const value_type& operator[](size_t i) const
+    {
+        auto it = constIterator(&data_[0]);
+        return *it[i];
+    }
+    
+    constIterator at(int i) const
+    {
+    	if (i >= int(size())) {
+		throw std::invalid_argument("Element not exist");
+	}
+        auto begin_it = begin();
+        auto end_it = end();
+        int count = 0;
+        while (begin_it != end_it)
+        {
+            if (count == i)
+            {
+                return begin_it;
+            }
+            ++begin_it;
+            count++;
+        }
+        return NULL;
+        
+    }
+    
+    value_type& front()
+    {
+        return data_[0];
+    }
+    
+    value_type& back()
+    {
+        return data_[N- 1];
+    }
+    
+    const value_type& frontConst() const
+    {
+        return data_[0];
+    }
+    
+    const value_type& backConst() const
+    {
+        return data_[N - 1];
+    }
+    
+    bool empty() const
+    {
+        return N == 0;
+    }
+    
+    size_t size() const
+    {
+        return N;
+    }
+
+    constexpr size_t sizeConstexpr() 
+    {
+        return N;
+    }
+    template <class U, size_t K>
+    friend std::ostream& operator<< (std::ostream& , MyArray<U, K>&);
+    
+};
+
+template <class T, size_t N>
+std::ostream& operator<< (std::ostream& out, MyArray<T, N>& arr)
 {
-    MyArray<int, MAX> arr({0, 0, 0, 0, 0, 0});
-    for (int i = 0; i < MAX; ++i) {
-        
-        arr[i] = i + 1;
-    }
-
-    int valueToCheck = 1;
-    for (auto it = arr.begin(), end = arr.end(); it != end; ++it) { 
-        auto i = *it; 
-        CHECK(i == valueToCheck);
-        valueToCheck++;
-    }
-   
+    for(size_t i = 0; i < arr.size(); ++i)
+        {
+            out << arr.data_[i] << " ";
+        };
+    return out;
 }
-
-
-TEST_CASE("CheckConstReverseIterator", "[array]")
-{
-    MyArray<int, MAX> arr({0, 0, 0, 0, 0, 0});
-    for (int i = 0; i < MAX; ++i) {
-        
-        arr[i] = i + 1;
-    }
-    int valueToCheck = 6;
-    for (auto it = arr.rbegin(), end = arr.rend(); it != end; ++it) { 
-        const auto i = *it; 
-        CHECK(i == valueToCheck);
-        valueToCheck--;
-    }
-   
-
-}
-
-
-
-
-TEST_CASE("CheckEmpty", "[array]")
-{
-    MyArray<int, MAX> arr({0, 0, 0, 0, 0, 0});
-    for (int i = 0; i < MAX; ++i) {
-        
-        arr[i] = i;
-    }
-    CHECK_FALSE(arr.empty());
-
-
-}
-
